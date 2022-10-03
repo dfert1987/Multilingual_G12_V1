bd8c879dc45eba89560ec1ecebbebf63ffd0a9bf
David Fertitta <Davidj.fertitta@gmail.com>
2022-10-03 10:28:04 -0500

    first

---
+++ src/Assets/Data/Unit6SelectionSupport.js
@@ -0,0 +1,85 @@
+export const Unit6SelectionSupport = [
+    {
+        Selection: 'A Cup of Tea',
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_ese_acupoftea_mls.pdf',
+        Summary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_satx_cupoftea_sa.pdf',
+        TextSketchEnglish: 'NA',
+        TextSketchSpanish: 'NA',
+    },
+    {
+        Selection: 'The Love Song of J. Alfred Prufrock',
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_ese_songprufrock_mls.pdf',
+        Summary: 'NA',
+        TextSketchEnglish: 'NA',
+        TextSketchSpanish: 'NA',
+    },
+    {
+        Selection: 'Shooting an Elephant',
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_ese_shootelephant_mls.pdf',
+        Summary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_satx_shootinganelephant_sa.pdf',
+        TextSketchEnglish:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_txts_shooting.pdf',
+        TextSketchSpanish:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_txts_shooting_sp.pdf',
+    },
+    {
+        Selection: 'My Daughter the Racist',
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_ese_mydaughter_mls.pdf',
+        Summary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_satx_mydaughterracist_sa.pdf',
+        TextSketchEnglish: 'NA',
+        TextSketchSpanish: 'NA',
+    },
+    {
+        Selection: 'The Second Coming',
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_ese_secondsymbols_mls.pdf',
+        Summary: 'NA',
+        TextSketchEnglish: 'NA',
+        TextSketchSpanish: 'NA',
+    },
+    {
+        Selection: "Symbols? I'm Sick of Symbols",
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_13_ese_secondsymbols_mls.pdf',
+        Summary: 'NA',
+        TextSketchEnglish: 'NA',
+        TextSketchSpanish: 'NA',
+    },
+    {
+        Selection: "Budget 2016: George Osborne's Speech",
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/read_studio/g12/student/pdf/12le_03_ese_U6_sp.pdf',
+        Summary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_satx_budget2016_sa.pdf',
+        TextSketchEnglish:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_txts_budgetsugartax.pdf',
+        TextSketchSpanish:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_txts_budgetsugartax_sp.pdf',
+    },
+    {
+        Selection: 'Will the Sugar Tax Stop Childhood Obesity?',
+        Excerpt: false,
+        MultilingualSummary:
+            'https://www.hmhco.com/content/literature/read_studio/g12/student/pdf/12le_03_ese_U6_sp.pdf',
+        Summary:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_satx_sugartaxobesity_sa.pdf',
+        TextSketchEnglish:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_txts_budgetsugartax.pdf',
+        TextSketchSpanish:
+            'https://www.hmhco.com/content/literature/into_lit/g12/student/pdf/12le_03_txts_budgetsugartax_sp.pdf',
+    },
+];
